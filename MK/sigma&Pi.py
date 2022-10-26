b8cdd9220c2e1ff04ad425a2ff005612603825a9
Muhammad Rofi <112401116+muhammadrofi12@users.noreply.github.com>
2022-10-26 15:57:08 +0700

    Update sigma&Pi.py

--- MK/sigma&Pi.py
+++ MK/sigma&Pi.py
@@ -1,4 +1,4 @@
-# Menghitung sigma (5i+2) dari 13 sampai 17
+#menghitung sigma (5i+2) dari 13 sampai 17
 hasil = 0
 n = 17
 for i in range(13, n+1):
@@ -6,7 +6,7 @@ for i in range(13, n+1):
 print(hasil)        #hasil bernilai 385
 
 
-# Menghitung sigma (n^2 + 2n - 1) dari 5 sampai 10
+#menghitung sigma (n^2 + 2n - 1) dari 5 sampai 10
 hasil2 = 0
 n2 = 10
 for m in range(5, n2+1):
@@ -14,7 +14,7 @@ for m in range(5, n2+1):
 print(hasil2)       #hasil bernilai 439
 
 
-# Menghitung notasi Pi (i-3) dari 4 sampai 8
+#menghitung notasi Pi (i-3) dari 4 sampai 8
 hasil3 = 1
 n3 = 8
 for k in range(4, n3+1):
